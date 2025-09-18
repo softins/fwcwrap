41625d46b582787aafeb97e86554bc25049d1776
Tony Mountifield <tony@mountifield.org>
2025-09-18 15:38:07 +0100

    Added -L option to list IP sources in a zone

--- fwcwrap.c
+++ fwcwrap.c
@@ -30,6 +30,7 @@ static void usage(char *prog)
 			"   -a IPaddr      IP address to add to zone\n"
 			"   -d IPaddr      IP address to delete from zone\n"
 			"   -q IPaddr      IP address to query from zone\n"
+			"   -L             List all IPs from zone\n"
 			"   -?             Help (this message)\n"
 			, prog);
 	exit(1);
@@ -40,10 +41,10 @@ int main(int argc, char *argv[])
 	extern char *optarg;
 	extern int optind;
 	int c;
-	int zoneopt = 0, ipadd = 0, ipdel = 0, ipquery = 0;
+	int zoneopt = 0, ipadd = 0, ipdel = 0, ipquery = 0, iplist = 0;
 	char zone[32],ipaddr[40];
 
-	while ((c = getopt(argc, argv, "z:d:a:q:?")) != EOF)
+	while ((c = getopt(argc, argv, "z:d:a:q:L?")) != EOF)
 		switch (c) {
 			case 'z':	/* name of zone */
 				if (strlen(optarg) >= sizeof(zone)) {
@@ -77,6 +78,9 @@ int main(int argc, char *argv[])
 				strcpy(ipaddr, optarg);
 				ipquery = 1;
 				break;
+			case 'L':	/* List IPs in zone */
+				iplist = 1;
+				break;
 			case '?':	/* help */
 				usage(argv[0]);
 				break;
@@ -94,24 +98,29 @@ int main(int argc, char *argv[])
 		usage(argv[0]);
 	}
 
-	if ((ipadd+ipdel+ipquery) > 1) {
-		fprintf(stderr, "%s: cannot specify more than one of -a, -d and -q\n", argv[0]);
+	if ((ipadd+ipdel+ipquery+iplist) > 1) {
+		fprintf(stderr, "%s: cannot specify more than one of -a, -d, -q and -L\n", argv[0]);
 		usage(argv[0]);
 	}
 
 	/* we deliberately don't use --permanent, so changes are only until reboot */
-	if (ipadd || ipdel || ipquery) {
+	if (ipadd || ipdel || ipquery || iplist) {
 		argv[0] = COMMAND;
-		argv[1] = "-q";
-		argv[2] = "--zone";
-		argv[3] = zone;
-		argv[4] = ipquery ? "--query-source" : ipadd ? "--add-source" : "--remove-source";
-		argv[5] = ipaddr;
-		argv[6] = NULL;
+		argv[1] = "--zone";
+		argv[2] = zone;
+		if (iplist) {
+			argv[3] = "--list-sources";
+			argv[4] = NULL;
+		} else {
+			argv[3] = "-q";
+			argv[4] = ipquery ? "--query-source" : ipadd ? "--add-source" : "--remove-source";
+			argv[5] = ipaddr;
+			argv[6] = NULL;
+		}
 
 		return cmd(argv);
 	} else {
-		fprintf(stderr, "%s: must specify either -a, -d or -q\n", argv[0]);
+		fprintf(stderr, "%s: must specify either -a, -d, -q or -L\n", argv[0]);
 		usage(argv[0]);
 	}
 
